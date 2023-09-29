df2a42a832264793aa67e1f9fada2bcc4d1b1401
fuaadtarmizi <fuaad2120@gmail.com>
2023-09-29 14:41:55 +0800

    updated

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
 import { Link } from "react-router-dom"
+import Carousel from '../components/Carousel'
 
 const PageIndex = () => {
   return (
     <>
       <h1 className="font-bold text-xxl">test</h1>
       <Link to="/inventory">Inventory</Link>
+      <Carousel/>
     </>
   )
 }
